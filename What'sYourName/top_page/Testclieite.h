53c10f46daf636bda49cecb4a0845bc6e5fe9486
Yoza <e125757@nw1257.st.ie.u-ryukyu.ac.jp>
2013-12-05 11:10:16 +0900

    add save function

---
+++ What'sYourName/top_page/Testclieite.h
@@ -0,0 +1,13 @@
+//
+//  Testclieite.h
+//  top_page
+//
+//  Created by Yoza on 2013/11/28.
+//  Copyright (c) 2013年 Yoza. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface Testclieite : UITableViewController
+
+@end
