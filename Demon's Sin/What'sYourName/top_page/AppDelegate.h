2450ad708bd4283d8e5b444234cc6a488f79376e
Yoza <e125757@nw1257.st.ie.u-ryukyu.ac.jp>
2013-12-05 11:13:18 +0900

    add my file

---
+++ Demon's Sin/What'sYourName/top_page/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  top_page
+//
+//  Created by Yoza on 2013/11/14.
+//  Copyright (c) 2013年 Yoza. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Demon's Sin/What'sYourName/top_pageTests/top_pageTests.m
@@ -0,0 +1,34 @@
+//
+//  top_pageTests.m
+//  top_pageTests
+//
+//  Created by Yoza on 2013/11/14.
+//  Copyright (c) 2013年 Yoza. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface top_pageTests : XCTestCase
+
+@end
+
+@implementation top_pageTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
