bc58c4314d6336a9ce42ce25b57a9ac197f1833a
知念 拓弥 <chinentakuya@nw1241.st.ie.u-ryukyu.ac.jp>
2014-01-16 03:37:32 +0900

    Business card 2nd

---
+++ chi-taku/What'sYourName/top_page/BusinessCard.h
@@ -0,0 +1,13 @@
+//
+//  BusinessCard.h
+//  top_page
+//
+//  Created by 知念 拓弥 on 2014/01/16.
+//  Copyright (c) 2014年 Yoza. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BusinessCard : UITableViewController
+
+@end
